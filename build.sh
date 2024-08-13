3be8948b13a6625ccd03a5c2b19c18a1aa9cd034
Christian Hagemeier <christian@hagemeier.ch>
2024-08-13 17:28:14 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+sudo apt install libtool-bin g++ automake cmake flex bison mona libboost-all-dev 
+cd external/cudd 
+autoreconf -i 
+./configure --enable-silent-rules --enable-obj --enable-dddmp 
+sudo make install 
+cd ../../Syft
+mkdir build && cd build
+cmake .. 
+cmake --build .
+
