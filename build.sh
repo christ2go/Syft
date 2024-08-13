35346d3a1f22aed897539a85716413a141bb5d11
Christian Hagemeier <christian@hagemeier.ch>
2024-08-13 17:31:01 +0100

    small update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 sudo apt install libtool-bin g++ automake cmake flex bison mona libboost-all-dev 
-cd external/cudd 
+cd ../external/cudd 
 autoreconf -i 
 ./configure --enable-silent-rules --enable-obj --enable-dddmp 
 sudo make install 
